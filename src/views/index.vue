9b196765e860e97cfb060de3e182b28793b29e4b
心灵的孤僻 <zhang15733@sina.cn>
2023-05-15 19:06:41 +0800

    feat: 更新首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h2>戏曲字幕</h2>
+    <van-image src="/logo.png" width="7rem" radius="16"/>
     <p>一款应用于戏曲直播时自动发送字幕的软件。</p>
   </header>
   <van-cell-group title="应用">
@@ -8,7 +8,7 @@
       center
       title="功能简介"
       value="查看"
-      to="xiquzimu/howToUse"
+      to="howToUse"
       is-link
     />
   </van-cell-group>
@@ -23,19 +23,19 @@
   <van-cell-group title="常见问题">
     <van-cell
       title="如何开启无障碍服务？"
-      to="xiquzimu/openAccesibilityService"
+      to="openAccesibilityService"
       value="查看"
       is-link
     />
-    <van-cell title=""/>
   </van-cell-group>
   <van-cell-group title="声明">
-    <van-cell title="戏曲字幕声明" to="xiquzimu/statement" value="查看" is-link />
+    <van-cell title="戏曲字幕声明" to="statement" value="查看" is-link />
   </van-cell-group>
 </template>
 <script setup lang="ts"></script>
 <style scoped>
 header {
+  margin-top: 30px;
   display: flex;
   align-items: center;
   flex-direction: column;
