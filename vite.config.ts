49fd696048559c9a243d78ad6ab8d81ea8ebe7cc
hzsn-csh <hzsn-csh@qq.com>
2023-05-22 14:59:50 +0800

    build: 0.0.8

--- vite.config.ts
+++ vite.config.ts
@@ -9,27 +9,10 @@ export default defineConfig({
   plugins: [
     vue(),
     legacy({
-      targets: ['defaults', 'ie >= 11', 'chrome 52'],  //需要兼容的目标列表，可以设置多个
+      targets: ['defaults', 'ie >= 11', 'Chrome >= 52'],  //需要兼容的目标列表，可以设置多个
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
       renderLegacyChunks: true,
-      polyfills: [
-        'es.symbol',
-        'es.array.filter',
-        'es.promise',
-        'es.promise.finally',
-        'es/map',
-        'es/set',
-        'es.array.for-each',
-        'es.object.define-properties',
-        'es.object.define-property',
-        'es.object.get-own-property-descriptor',
-        'es.object.get-own-property-descriptors',
-        'es.object.keys',
-        'es.object.to-string',
-        'web.dom-collections.for-each',
-        'esnext.global-this',
-        'esnext.string.match-all'
-      ]
+      modernPolyfills: true
     }),
     Components({
       resolvers: [VantResolver()],
