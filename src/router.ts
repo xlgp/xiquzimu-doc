4eba9283607f38f8f8827e565ee12537ef55ea11
hzsn-csh <hzsn-csh@qq.com>
2023-06-25 14:04:40 +0800

    fix: 无法找到资源跳回首页

--- src/router.ts
+++ src/router.ts
@@ -2,14 +2,15 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Index from "./views/index.vue"
 
 const routes = [
-    { path: '/', component: Index, meta: { title: "首页" } },
+    { path: '/', name: "Index", component: Index, meta: { title: "首页" } },
     { path: "/openAccesibilityService", name: "openAccesibilityService", meta: { title: "如何开启无障碍服务" }, component: () => import("./views/helpCenter/openAccesibilityService.vue") },
     { path: "/WhyIsAutoCloseAccesibilityService", name: "WhyIsAutoCloseAccesibilityService", meta: { title: "为什么无障碍服务会自动关闭" }, component: () => import("./views/helpCenter/WhyIsAutoCloseAccesibilityService.vue") },
     { path: "/agreement", name: "agreement", meta: { title: "用户协议" }, component: () => import("./views/agreement-privacy/agreement.vue") },
     { path: "/privacy", name: "privacy", meta: { title: "隐私政策" }, component: () => import("./views/agreement-privacy/privacy.vue") },
     { path: "/download", name: "download", meta: { title: "应用下载" }, component: () => import("./views/download/index.vue") },
-    { path: "/howToUse", name: "howToUse", meta: { title: "功能简介" }, component: () => import("./views/features/index.vue") }
-
+    { path: "/howToUse", name: "howToUse", meta: { title: "功能简介" }, component: () => import("./views/features/index.vue") },
+    //跳回首页
+    { path: '/:pathMatch(.*)*', name: 'NotFound', redirect: "/" },
 ]
 
 export default createRouter({
