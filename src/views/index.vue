22675bb5af71ca4919800b6a03d4f0f7750d24ba
心灵的孤僻 <zhang15733@sina.cn>
2023-06-30 21:00:41 +0800

    feat: 高亮选中行

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,31 +4,17 @@
     <div class="van-cell-group__title">一款应用于戏曲直播时自动发送字幕的软件。</div>
   </header>
   <van-cell-group title="应用">
-    <van-cell center title="功能简介" value="查看" to="howToUse" is-link />
-    <van-cell center title="戏曲字幕" value="去下载" to="download" is-link />
+    <van-cell title="功能简介" value="查看" to="howToUse" is-link />
+    <van-cell :class="{ 'tip-bg': isDownload }" title="戏曲字幕" value="去下载" to="download" is-link />
   </van-cell-group>
   <van-cell-group title="唱段">
-    <van-cell
-      title="唱段列表"
-      value="查看"
-      url="https://gitee.com/xlgp/opera-lyrics/tree/master"
-      is-link
-    />
+    <van-cell title="唱段列表" value="查看" url="https://gitee.com/xlgp/opera-lyrics/tree/master" is-link />
   </van-cell-group>
   <van-cell-group title="常见问题">
-    <van-cell
-      title="如何开启无障碍服务？"
-      to="openAccesibilityService"
-      value="查看"
-      is-link
-    />
-    <van-cell
-      title-class="zimu-van-cell-title"
-      title="为什么无障碍服务会自动关闭？"
-      to="WhyIsAutoCloseAccesibilityService"
-      value="查看"
-      is-link
-    />
+    <van-cell :class="{ 'tip-bg': isOpenAccesibilityService }" title="如何开启无障碍服务？" to="openAccesibilityService" value="查看"
+      is-link />
+    <van-cell title-class="zimu-van-cell-title" title="为什么无障碍服务会自动关闭？" to="WhyIsAutoCloseAccesibilityService" value="查看"
+      is-link />
   </van-cell-group>
   <van-cell-group title="用户协议和隐私政策">
     <van-cell title="用户协议" to="agreement" value="查看" is-link />
@@ -36,6 +22,13 @@
   </van-cell-group>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue';
+
+import { useRoute } from 'vue-router';
+
+const { query } = useRoute();
+const isOpenAccesibilityService = computed(() => query.item && query.item == 'openAccesibilityService');
+const isDownload = computed(() => query.item && query.item == "download");
 </script>
 <style scoped>
 header {
@@ -43,6 +36,21 @@ header {
   display: flex;
   align-items: center;
   flex-direction: column;
+}
+
+@keyframes tips {
+  from {
+    background: #d4b6ff;
+  }
+
+  to {
+    background: var(--van-cell-background);
+  }
+}
+
+.tip-bg {
+
+  animation: tips 2s ease-in-out 0s 2;
 }
 </style>
 <style>
