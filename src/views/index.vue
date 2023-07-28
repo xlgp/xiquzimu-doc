b31c027bde7c866e801ab78bd0dd914743859a83
心灵的孤僻 <zhang15733@sina.cn>
2023-07-28 22:06:06 +0800

    feat: 更新首页 更改logo

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,27 @@
 <template>
   <header>
+    <van-image width="100" height="100" src="/logo.svg" />
     <h2>戏曲字幕服务中心</h2>
-    <div class="van-cell-group__title">一款应用于戏曲直播时自动发送字幕的软件。</div>
+    <div class="van-cell-group__title">
+      一款应用于戏曲直播时自动发送字幕的软件。
+    </div>
   </header>
+  <van-row gutter="20">
+    <van-col span="12">
+      <div class="x-van-col" :class="{ 'tip-bg': isDownload }" @click="toDownload">
+        <van-icon name="down" size="2rem" class="col-icon" />
+        <div class="icon-text">去下载</div>
+      </div>
+    </van-col>
+    <van-col span="12">
+      <div class="x-van-col" @click="toChangDuan">
+        <van-icon name="/ic_changci.svg" size="2rem" class="col-icon" />
+        <div class="icon-text">唱段</div>
+      </div>
+    </van-col>
+  </van-row>
   <van-cell-group title="应用">
     <van-cell title="功能简介" value="查看" to="howToUse" is-link />
-    <van-cell :class="{ 'tip-bg': isDownload }" title="戏曲字幕" value="去下载" to="download" is-link />
-  </van-cell-group>
-  <van-cell-group title="唱段">
-    <van-cell title="唱段列表" value="查看" url="https://gitee.com/xlgp/opera-lyrics/tree/master" is-link />
   </van-cell-group>
   <van-cell-group title="常见问题">
     <van-cell :class="{ 'tip-bg': isOpenAccesibilityService }" title="如何开启无障碍服务？" to="openAccesibilityService" value="查看"
@@ -24,13 +37,45 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const { query } = useRoute();
 const isOpenAccesibilityService = computed(() => query.item && query.item == 'openAccesibilityService');
 const isDownload = computed(() => query.item && query.item == "download");
+
+const giteeChangDuanUrl = "https://gitee.com/xlgp/opera-lyrics/tree/master";
+
+const router = useRouter();
+
+const toDownload = () => {
+  router.push("/download");
+}
+
+const toChangDuan = () => {
+  window.location.href = giteeChangDuanUrl;
+}
+
 </script>
 <style scoped>
+.x-van-col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 16px;
+  margin: 10px;
+  flex-direction: column;
+  background-color: #f7f7f7;
+  padding-bottom: 20px;
+}
+
+.icon-text {
+  color: var(--van-cell-text-color);
+}
+
+.col-icon {
+  padding: 20px;
+}
+
 header {
   margin-top: 30px;
   display: flex;
